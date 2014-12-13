87c6c2d345c1cb284d4dfa6a1e42566faf991651
The Realist <schtherealist@hotmail.com>
2014-12-12 20:10:42 -0600

    Create achievement.lua

---
+++ lua/autorun/achievement.lua
@@ -0,0 +1,16 @@
+function ulx.achievement( calling_ply, target_plys, name )
+
+	for _,x in pairs (player.GetAll()) do
+		for k,v in pairs (target_plys) do
+			ULib.tsayColor( v, false, team.GetColor(v), v:Nick(), Color(255,255,255,255), " earned the achievement ", Color(255,200,0,255), name)
+		end
+	end
+	
+	ULib.tsay( _, "You faked the achievement " .. name .. " for the players you selected.")
+
+end
+local achievement = ulx.command( "Prop Hunt", "ulx achievement", ulx.achievement, "#ach", true, true )
+achievement:defaultAccess( ULib.ACCESS_ADMIN )
+achievement:addParam{ type=ULib.cmds.PlayersArg }
+achievement:addParam{ type=ULib.cmds.StringArg, hint="name", ULib.cmds.takeRestOfLine }
+achievement:help( "Fakes an earned achievement" )
